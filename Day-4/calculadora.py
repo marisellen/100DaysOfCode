5830faa1e1f1bf206b7e727c34a14b1fb816fd07
marianaselene <mariana02301@gmail.com>
2025-09-05 21:46:48 -0300

    day3

---
+++ Day-4/calculadora.py
@@ -0,0 +1,22 @@
+def adicao(x,y):
+    return x+y
+
+def subtracao(x,y):
+    return x-y
+
+def multiplicacao(x,y):
+    return x*y
+
+def divisao(x,y):
+    return x/y
+
+def calculçadora():
+    print("Selecione a operação:")
+    print("1 - Adição")
+    print("2 - Subtração")
+    print("3 - Multiplicação")
+    print("4 - Divisão")
+
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+
---
+++ Day-5/calculadora.py
@@ -0,0 +1,44 @@
+def adicao(x,y):
+    return x+y
+
+def subtracao(x,y):
+    return x-y
+
+def multiplicacao(x,y):
+    return x*y
+
+def divisao(x,y):
+    return x/y
+
+def calculçadora():
+    print("Selecione a operação:")
+    print("1 - Adição")
+    print("2 - Subtração")
+    print("3 - Multiplicação")
+    print("4 - Divisão")
+
+while True:
+    escolha = input("Escolha(1/2/3/4):")
+    if escolha in ("1", "2", "3", "4"):
+        num1 = int(input("Digite o primeiro número: "))
+        num2 = int(input("Digite o segundo número: "))
+        if escolha == '1':
+            print("Resultado: ", adicao(num1, num2))
+        if escolha == '2':
+            print("Resultado: ", subtracao(num1, num2))
+        if escolha == '3':
+            print("Resultado: ", multiplicacao(num1, num2))
+        if escolha == '4':
+            if num2 != 0:
+                print("Resultado: ", divisao(num1, num2))
+            else:
+                print("Não é possivel dividir por zero")
+    else:
+        print("Escolha invalida.")
+
+    continuar = input("Deseja fazer outra operação? (s/n): ")
+    if continuar == "n":
+        print("Calculadora encerrada")
+        break
+
+calculçadora()
